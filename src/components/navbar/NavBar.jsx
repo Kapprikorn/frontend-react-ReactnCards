b251e381cf28a44fa0b3ee6ce2595b6868b33488
Kapprikorn <sjors2202@hotmail.com>
2024-10-27 14:27:49 +0100

    Created navBar component. Added dropdown list with pages. Added navigation to dropdown items.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,20 +1,66 @@
-// import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from './NavBar.module.css';
 
-import logo from '../../../public/vite.svg';
+import logo from '../../assets/React_n_Cards_logo.png';
+import Button from '../button/Button.jsx';
 
-function NavBar() {
+function NavBar({ pageName }) {
   const navigate = useNavigate();
-  // const [activePage, setActivePage] = useState('');
+  // TODO: implement navigation and pageName logic.
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
+  const handleLogout = () => {
+    // TODO: implement logout logic.
+    handleNavigation('/');
+  }
 
   return (
-    <nav className="">
-      <div className="navigation-logo-wrapper">
-        <img className="navigation-logo"
-             src={logo}
-             alt="App logo"
-             onClick={() => navigate('/')} />
+    <nav className={styles.navbar}>
+      <div className={styles.leftSection}>
+        <img
+          className={styles.logo}
+          src={`${logo}`}
+          alt="App logo"
+          onClick={() => {navigate('/home'); }}
+        />
+        <div className={styles.dropdownContainer}>
+          <button className={styles.dropdownToggleButton}>
+            Games ▼
+          </button>
+          <ul className={styles.dropdownMenu} role="menu">
+            <li>
+              <button
+                className={styles.dropdownItem}
+                onClick={() => handleNavigation('/hilo')}
+                role="menuitem"
+              >
+                HiLo
+              </button>
+            </li>
+            <li>
+              <button
+                className={styles.dropdownItem}
+                onClick={() => handleNavigation('/blackjack')}
+                role="menuitem"
+              >
+                Blackjack
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
+
+      <h1 className={styles.pageName}>
+        {pageName}
+      </h1>
+
+      <Button
+        handleClick={handleLogout}
+        text="Logout"
+      />
     </nav>
   );
 }
