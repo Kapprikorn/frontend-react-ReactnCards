a02134374d73338a34bbf02f2f68e975c1a9e3e7
Kapprikorn <sjors2202@hotmail.com>
2024-10-28 09:29:38 +0100

    Adjusted Home page for use with the GamePreview component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+import styles from './Home.module.css';
+import hilo_preview from '../../assets/HiLo_preview.png';
+import blackjack_preview from '../../assets/blackjack_preview.png';
+import GamePreview from '../../components/gamePreview/GamePreview.jsx';
 
 function Home() {
   return (
-    <div className="main-content">
-
-
+    <div className={styles.homeScreenWrapper}>
+      <GamePreview title="HiLo" image={hilo_preview} route="/hilo" />
+      <GamePreview title="Blackjack" image={blackjack_preview} route="/blackjack" />
     </div>
   );
 }
